7dc402be0b3335dc25deff296d555817634de736
Hemal Patel <hemal.patel7735@outlook.com>
2019-11-26 10:52:51 +0530

    update(hamming): use rune instead of string length

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -4,14 +4,16 @@ import "errors"
 
 // Distance returns the hamming_distance, error
 func Distance(a, b string) (int, error) {
-
-	if len(a) != len(b) {
+	runeA := []rune(a)
+	runeB := []rune(b)
+	
+	if len(runeA) != len(runeB) {
 		return 0, errors.New("Length of a and Length of b does not match")
 	}
 
 	dist := 0
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
+	for i := 0; i < len(runeA); i++ {
+		if runeA[i] != runeB[i] {
 			dist++
 		}
 	}
